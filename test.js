9ff407d7c0cc9c6a236e0f01b23489cf58579553
Mango0x45 <thomasvoss@live.com>
2020-10-20 23:21:09 +0200

    Remove test files

--- test.js
+++
@@ -1,9 +0,0 @@
-var sqlite3 = require("sqlite3").verbose();
-var file = "./data/leaderboard";
-var db = new sqlite3.Database(file);
-db.all("SELECT rowid, * FROM runner", function (err, rows) {
-	rows.forEach(function (row) {
-		console.log(row.rowid, row.name, row.nationality);
-	});
-});
-db.close();
