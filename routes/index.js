d3f98f823010a08fb6b0b588ec94d524c4a4b0f5
Mango0x45 <thomasvoss@live.com>
2020-10-24 20:43:26 +0200

    Show recent records on leaderboardhome

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,21 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
+// Convert seconds to human readable time
 function timeFormat(duration) {
-	// Hours, minutes and seconds
-	var hrs = ~~(duration / 3600);
-	var mins = ~~((duration % 3600) / 60);
-	var secs = ~~duration % 60;
+	const hours = ~~(duration / 3600);
+	const minutes = ~~((duration % 3600) / 60);
+	const seconds = ~~duration % 60;
+	let output = "";
 
-	// Output like "1:01" or "4:03:59" or "123:03:59"
-	var ret = "";
-
-	if (hrs > 0) {
-		ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+	if (hours > 0) {
+		output += "" + hours + ":" + (minutes < 10 ? "0" : "");
 	}
 
-	ret += "" + mins + ":" + (secs < 10 ? "0" : "");
-	ret += "" + secs;
-	return ret;
+	output += "" + minutes + ":" + (seconds < 10 ? "0" : "");
+	output += "" + seconds;
+
+	return output;
 }
 
 /* GET home page. */
@@ -32,50 +31,72 @@ router.get("/home", function (req, res, next) {
 router.get("/leaderboard/:cat?", function (req, res, next) {
 	const db = req.app.get("leaderboard");
 
-	if (typeof req.params.cat === "undefined") {
-		res.send(req.headers);
-		//res.render("leaderboardhome");
-	}
+    // If no category is specified, go to the leaderboard home page
+	if (typeof(req.params.cat) === "undefined") {
 
-	db.all(
-		"SELECT date, time, name, nationality, platform, version, link FROM runners, runs, pairs WHERE runners.rowid = runner_id AND runs.rowid = run_id AND category = ? ORDER BY date ASC",
-		[req.params.cat],
-		function (err, rows) {
-			// Calculate the duration of each run
-			for (i = 0, len = rows.length; i < len; i++) {
-				for (j = i + 1; j <= len; j++) {
-					if (j === len) {
-						rows[i].duration = Date.now() / 1000 - rows[i].date;
-					} else if (rows[j].time != rows[i].time) {
-						rows[i].duration = rows[j].date - rows[i].date;
-						break;
-					}
-				}
-				// Properly format the runs date, time, and duration
-				rows[i].date = new Date(rows[i].date * 1000).toLocaleDateString(
-					req.headers["accept-language"].substr(0, 5)
-					//"en-GB"
-				);
-				rows[i].time = timeFormat(rows[i].time);
-				rows[i].duration = Math.trunc(rows[i].duration / 86400);
+        // Get the 10 most recent world record runs, sorted by date
+        db.all("SELECT date, readable, time, name, nationality FROM runners, runs, pairs, categories WHERE runners.rowid = runner_id AND runs.rowid = run_id AND abbreviation = category ORDER BY date DESC LIMIT 10", [], function (err, recent) {
 
-				if (rows[i].duration === 0) {
-					rows[i].duration = "<1";
-				}
-			}
+            // For each run, format the date and time appropriately
+            for (i = 0, len = recent.length; i < len; i++) {
+                recent[i].date = new Date(recent[i].date * 1000).toLocaleDateString(
+                    req.headers["accept-language"].substr(0, 5)
+                );
 
-			db.get(
-				"SELECT name FROM categories WHERE abbreviation = ?",
-				[req.params.cat],
-				function (err, category) {
-					res.render("leaderboard", {
-						category: category.name,
-						leaderboard: rows,
-					});
-				}
-			);
-		}
-	);
+                recent[i].time = timeFormat(recent[i].time);
+            }
+
+            res.render("leaderboardhome", {
+                recent: recent,
+            });
+        });
+	} else {
+        // Query all the runs for the specified category, sorted by date
+        db.all(
+            "SELECT date, time, name, nationality, platform, version, link FROM runners, runs, pairs WHERE runners.rowid = runner_id AND runs.rowid = run_id AND category = ? ORDER BY date ASC",
+            [req.params.cat],
+            function (err, rows) {
+                // Calculate the duration of each run
+                for (i = 0, len = rows.length; i < len; i++) {
+                    // Check all the more recent records until a faster one is found
+                    // Can't just check the very next because of the possibility of ties
+                    for (j = i + 1; j <= len; j++) {
+                        // Check if the record is current
+                        if (j === len) {
+                            rows[i].duration = Date.now() / 1000 - rows[i].date;
+                        } else if (rows[j].time != rows[i].time) {
+                            rows[i].duration = rows[j].date - rows[i].date;
+                            break;
+                        }
+                    }
+
+                    // Properly format the runs date, time, and duration
+                    rows[i].date = new Date(rows[i].date * 1000).toLocaleDateString(
+                        req.headers["accept-language"].substr(0, 5) // "en-GB"
+                    );
+
+                    rows[i].time = timeFormat(rows[i].time);
+                    rows[i].duration = Math.trunc(rows[i].duration / 86400);
+
+                    if (rows[i].duration === 0) {
+                        rows[i].duration = "<1";
+                    }
+                }
+
+                // Get the category name
+                db.get(
+                    "SELECT readable FROM categories WHERE abbreviation = ?",
+                    [req.params.cat],
+                    function (err, category) {
+                        res.render("leaderboard", {
+                            category: category.name,
+                            leaderboard: rows,
+                        });
+                    }
+                );
+            }
+        );
+    }
 });
 
 router.get("/about", function (req, res, next) {
