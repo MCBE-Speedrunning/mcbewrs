02424d253889f9354d25dfeb988ed2f83cb157a2
Mango0x45 <thomasvoss@live.com>
2020-11-30 15:42:30 +0100

    Aligned spacing pls

--- utils/setup_data.sh
+++ utils/setup_data.sh
@@ -11,15 +11,15 @@ DATA="${SCRIPT_PATH}/../data"
 
 sqlite3 ${DATA}/auth.db << EOF
 CREATE TABLE IF NOT EXISTS config (
-	db_secret       TEXT NOT NULL,
-	server_port     INTEGER NOT NULL
+    db_secret       TEXT NOT NULL,
+    server_port     INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS users (
-	username        TEXT NOT NULL UNIQUE,
-	password        TEXT NOT NULL,
-	salt            TEXT NOT NULL,
-	PRIMARY KEY (username)
+    username        TEXT NOT NULL UNIQUE,
+    password        TEXT NOT NULL,
+    salt            TEXT NOT NULL,
+    PRIMARY KEY (username)
 );
 EOF
 
@@ -31,33 +31,33 @@ CREATE TABLE IF NOT EXISTS runners (
 );
 
 CREATE TABLE IF NOT EXISTS categories (
-	id				INTEGER PRIMARY KEY AUTOINCREMENT,
-	readable		TEXT NOT NULL UNIQUE,
-	abbreviation	TEXT NOT NULL UNIQUE,
-	type			TEXT NOT NULL DEFAULT 'main',
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    readable        TEXT NOT NULL UNIQUE,
+    abbreviation    TEXT NOT NULL UNIQUE,
+    type            TEXT NOT NULL DEFAULT 'main',
     corder          INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS runs (
-	id				INTEGER PRIMARY KEY AUTOINCREMENT,
-	category_id		INTEGER NOT NULL,
-	date			INTEGER NOT NULL,
-	time			NUMERIC NOT NULL,
-	duration		INTEGER NOT NULL,
-	platform		TEXT NOT NULL DEFAULT '-',
-	seed			TEXT NOT NULL DEFAULT '-',
-	version			TEXT NOT NULL DEFAULT '-',
-	input			TEXT NOT NULL DEFAULT '-',
-	link			TEXT,
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    category_id     INTEGER NOT NULL,
+    date            INTEGER NOT NULL,
+    time            NUMERIC NOT NULL,
+    duration        INTEGER NOT NULL,
+    platform        TEXT NOT NULL DEFAULT '-',
+    seed            TEXT NOT NULL DEFAULT '-',
+    version         TEXT NOT NULL DEFAULT '-',
+    input           TEXT NOT NULL DEFAULT '-',
+    link            TEXT,
     wr              INTEGER NOT NULL DEFAULT 0,
-	FOREIGN KEY (category_id) REFERENCES categories(id)
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
 
 CREATE TABLE IF NOT EXISTS pairs (
-	run_id          INTEGER NOT NULL,
-	runner_id       INTEGER NOT NULL,
-	FOREIGN KEY (run_id) REFERENCES runs(id),
-	FOREIGN KEY (runner_id) REFERENCES runners(id)
+    run_id          INTEGER NOT NULL,
+    runner_id       INTEGER NOT NULL,
+    FOREIGN KEY (run_id) REFERENCES runs(id),
+    FOREIGN KEY (runner_id) REFERENCES runners(id)
 );
 EOF
 
