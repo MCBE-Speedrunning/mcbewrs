66cd6dae2e5b3df0b9a2c64894bda712e4255ee9
Mango0x45 <thomasvoss@live.com>
2021-01-08 18:04:14 +0100

    Add reformating for shell scripts

--- src/utils/format.sh
+++ src/utils/format.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env sh
 
 # Format all typescript/javascript files with clang-format
+# and use tabs instead of spaces in shell scripts
 
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 
 for FILE in $(find "$SCRIPT_PATH/../../" | grep "\.[jt]s$" | grep -v "node_modules"); do
-    clang-format --verbose -i -style=file "$FILE"
+	clang-format --verbose -i -style=file "$FILE"
+done
+
+# Unexpanding and redirecting into the same file erases
+# all the contents, so we redirect into a temporary file
+# and then rename it, but that requires marking it as
+# executable again.
+for FILE in "$SCRIPT_PATH"/*.sh; do
+	echo Formatting "$FILE"
+	unexpand -t 4 "$FILE" >temp
+	mv temp "$FILE"
+	chmod +x "$FILE"
 done
