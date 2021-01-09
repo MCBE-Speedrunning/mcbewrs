23ffc5106c9fc418bdd7efc88886426a5c863f6f
Mango0x45 <thomasvoss@live.com>
2021-01-10 00:43:44 +0100

    Reformatted

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,25 +1,23 @@
-import createError from "http-errors";
 import {exec} from "child_process";
 import csurf from "csurf";
 import express, {NextFunction, Request, Response} from "express";
+import createError from "http-errors";
 import path from "path";
 import hashFunc from "pbkdf2-password";
+import {Database, open} from "sqlite";
 import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
 
 const hash = hashFunc();
 const router = express.Router();
 let leaderboard: Database<sqlite3.Database, sqlite3.Statement>;
-let db: Database<sqlite3.Database, sqlite3.Statement>; 
+let db: Database<sqlite3.Database, sqlite3.Statement>;
 (async () => {
 	leaderboard = await open({
 		filename: path.join(__dirname, "..", "data", "leaderboard.db"),
 		driver: sqlite3.cached.Database
 	});
-	db = await open({
-		filename: path.join(__dirname, "..", "data", "auth.db"),
-		driver: sqlite3.cached.Database
-	});
+	db = await open(
+	    {filename: path.join(__dirname, "..", "data", "auth.db"), driver: sqlite3.cached.Database});
 })();
 
 /*
@@ -51,25 +49,25 @@ function restrict(req: Request, res: Response, next: NextFunction) {
  */
 async function authenticate(name: string, pass: string, fn: (err: Error|null, user?: any) => void) {
 	const user = await db.get("SELECT * FROM users WHERE username = ?", name);
-		// Query the db for the given username
-		if (!user)
-			return fn(new Error("cannot find user"));
-		// Apply the same algorithm to the POSTed password, applying
-		// the hash against the pass / salt, if there is a match we
-		// found the user
-		hash({password: pass, salt: user.salt}, (err, _pass, _salt, hash) => {
-			if (err)
-				return fn(err);
-			if (hash === user.password)
-				return fn(null, user);
-			fn(new Error("invalid password"));
+	// Query the db for the given username
+	if (!user)
+		return fn(new Error("cannot find user"));
+	// Apply the same algorithm to the POSTed password, applying
+	// the hash against the pass / salt, if there is a match we
+	// found the user
+	hash({password: pass, salt: user.salt}, (err, _pass, _salt, hash) => {
+		if (err)
+			return fn(err);
+		if (hash === user.password)
+			return fn(null, user);
+		fn(new Error("invalid password"));
 	});
 }
 
 router.use(csurf({cookie: false}));
 
 router.get("/login",
-	(req, res) => { res.render("login", {session: req.session, csrfToken: req.csrfToken()}); });
+    (req, res) => { res.render("login", {session: req.session, csrfToken: req.csrfToken()}); });
 
 router.post("/login", async (req, res, next) => {
 	await authenticate(req.body.username, req.body.password, (err, user) => {
@@ -100,7 +98,7 @@ router.get("/logout", (req, res) => {
 });
 
 router.get(
-	"/register", restrict, (req, res) => { res.render("register", {csrfToken: req.csrfToken()}); });
+    "/register", restrict, (req, res) => { res.render("register", {csrfToken: req.csrfToken()}); });
 
 router.post("/register", restrict, (req, res) => {
 	newUser(req.body.username, req.body.password, (err) => { res.render("register", {err: err}); });
@@ -125,42 +123,34 @@ router.post("/add", restrict, async (req, res, next) => {
 
 	// All input from the client comes as a string, so everything must be parsed as an int
 	run.time = parseInt(run.hour, 10) * 60 * 60 + parseInt(run.minutes, 10) * 60
-			   + parseInt(run.seconds, 10);
+	           + parseInt(run.seconds, 10);
 
 	// Add 0.0001 to the end of runs that time with milliseconds
 	// This ensures that the site will display 3 significant figures
 	if (parseInt(run.milliseconds, 10))
 		run.time += parseInt(run.milliseconds, 10) / 1000 + 0.0001;
 
-		await leaderboard.run("INSERT INTO runs VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-				run.category_id,
-				run.date,
-				run.time,
-				// Run duration
-				0,
-				run.platform,
-				run.seed,
-				run.version,
-				run.input,
-				run.link,
-				// WR flag
-				0
-			)
+	await leaderboard.run("INSERT INTO runs VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+	    run.category_id, run.date, run.time,
+	    // Run duration
+	    0, run.platform, run.seed, run.version, run.input, run.link,
+	    // WR flag
+	    0)
 
-		// Prevents DoS
-		if (!(run.runners instanceof Array))
-			return [];
+	// Prevents DoS
+	if (!(run.runners instanceof Array))
+	return [];
 
-		// Insert the run/runner pairs
-				for (const runner of run.runners) 
-					await leaderboard.run(`INSERT INTO pairs VALUES(
+	// Insert the run/runner pairs
+	for (const runner of run.runners)
+		await leaderboard.run(`INSERT INTO pairs VALUES(
 						(SELECT MAX(id) FROM runs),
 						(SELECT id FROM runners WHERE name = ?)
 						)`,
-					runner);
+		    runner);
 
-		// Update the WR flags
-		await leaderboard.run(`UPDATE runs
+	// Update the WR flags
+	await leaderboard.run(`UPDATE runs
 						SET wr = CASE WHEN time = (
 								SELECT time FROM runs
 								WHERE category_id = ?
@@ -168,59 +158,58 @@ router.post("/add", restrict, async (req, res, next) => {
 						)
 						THEN 1 ELSE 0
 						END`,
-			run.category_id);
+	    run.category_id);
 
-		// Calculate the duration of each run in the category just updated
-		const rows = await leaderboard.all(`SELECT id, date, time FROM runs
+	// Calculate the duration of each run in the category just updated
+	const rows = await leaderboard.all(`SELECT id, date, time FROM runs
 						WHERE category_id = ?
 						ORDER BY date ASC`,
-			run.category_id);
-				for (let i = 0, len = rows.length; i < len; i++) {
-					// Check every newer record until a faster one is found
-					// Can't just check the very next because of ties
-					for (let j = i + 1; j <= len; j++) {
-						// Check if the record is current
-						if (j === len) {
-							rows[i].duration = Date.now() / 1000 - rows[i].date;
-						} else if (rows[j].time != rows[i].time) {
-							rows[i].duration = rows[j].date - rows[i].date;
-							break;
-						}
-					}
+	    run.category_id);
+	for (let i = 0, len = rows.length; i < len; i++) {
+		// Check every newer record until a faster one is found
+		// Can't just check the very next because of ties
+		for (let j = i + 1; j <= len; j++) {
+			// Check if the record is current
+			if (j === len) {
+				rows[i].duration = Date.now() / 1000 - rows[i].date;
+			} else if (rows[j].time != rows[i].time) {
+				rows[i].duration = rows[j].date - rows[i].date;
+				break;
+			}
+		}
 
-					// Add the runs duration to the DB
-					await leaderboard.run(`UPDATE runs SET duration = ? WHERE id = ?`,
-						rows[i].duration, rows[i].id);
-				}
+		// Add the runs duration to the DB
+		await leaderboard.run(
+		    `UPDATE runs SET duration = ? WHERE id = ?`, rows[i].duration, rows[i].id);
+	}
 
-		const categories = await leaderboard.all("SELECT * FROM categories");
-			res.render("admin_add", {
-				banner: {
-					text: "Run added succesfully",
-					status: "success",
-					csrfToken: req.csrfToken(),
-				},
-				categories: categories,
-				csrfToken: req.csrfToken(),
-			});
-		});
+	const categories = await leaderboard.all("SELECT * FROM categories");
+	res.render("admin_add", {
+		banner: {
+			text: "Run added succesfully",
+			status: "success",
+			csrfToken: req.csrfToken(),
+		},
+		categories: categories,
+		csrfToken: req.csrfToken(),
+	});
+});
 
 router.post("/new_user", restrict, async (req, res, next) => {
 	let username: string;
 	if (req.body.name)
 		username = req.body.name;
-	else 
+	else
 		return createError(405, "No name provided");
 	const nationality = req.body.nationality || null;
-	await db.run(
-		`INSERT INTO runners (name, nationality) VALUES(?, ?)`, username, nationality);
-			res.render("new_user", {
-				banner: {
-					text: "Runner added succesfully",
-					status: "success",
-					csrfToken: req.csrfToken(),
-				},
-		});
+	await db.run(`INSERT INTO runners (name, nationality) VALUES(?, ?)`, username, nationality);
+	res.render("new_user", {
+		banner: {
+			text: "Runner added succesfully",
+			status: "success",
+			csrfToken: req.csrfToken(),
+		},
+	});
 });
 
 router.get("/pull", restrict, (_req, res, next) => {
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,27 +4,25 @@ import createError from "http-errors";
 import jwt from "jsonwebtoken";
 import path from "path";
 import hashFunc from "pbkdf2-password";
+import {Database, open} from "sqlite";
 import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
 import xml from "xml";
 import {stringify} from "yaml";
 
 const hash = hashFunc();
 const router = express.Router();
 let leaderboard: Database<sqlite3.Database, sqlite3.Statement>;
-let auth: Database<sqlite3.Database, sqlite3.Statement>; 
+let auth: Database<sqlite3.Database, sqlite3.Statement>;
 (async () => {
 	leaderboard = await open({
 		filename: path.join(__dirname, "..", "data", "leaderboard.db"),
 		driver: sqlite3.cached.Database
 	});
-	auth = await open({
-		filename: path.join(__dirname, "..", "data", "auth.db"),
-		driver: sqlite3.cached.Database
-	});
+	auth = await open(
+	    {filename: path.join(__dirname, "..", "data", "auth.db"), driver: sqlite3.cached.Database});
 })();
 const config =
-	JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", "config.json"), "utf-8"));
+    JSON.parse(fs.readFileSync(path.join(__dirname, "..", "data", "config.json"), "utf-8"));
 /*
  * username is in the form { username: "my cool username" }
  * ^^the above object structure is completely arbitrary
@@ -77,47 +75,47 @@ function parseData(req: Request, res: Response, rows: any) {
 }
 
 router.get("/history", async (req, res) => {
-	const rows = await leaderboard.all("SELECT * FROM runs WHERE id BETWEEN ? AND ?",
-		req.query.min || 0, req.query.max || 10);
+	const rows = await leaderboard.all(
+	    "SELECT * FROM runs WHERE id BETWEEN ? AND ?", req.query.min || 0, req.query.max || 10);
 	parseData(req, res, rows);
 });
 
 router.get("/categories", async (req, res) => {
 	const rows = await leaderboard.all("SELECT * FROM categories WHERE id BETWEEN ? AND ?",
-		req.query.min || 0, req.query.max || 10)
+	    req.query.min || 0, req.query.max || 10)
 	parseData(req, res, rows);
 });
 
 router.get("/runners", async (req, res, next) => {
-	const rows = await leaderboard.all("SELECT * FROM runners WHERE id BETWEEN ? and ?",
-		req.query.min || 0, req.query.max || 10)
-			parseData(req, res, rows);
-		});
+	const rows = await leaderboard.all(
+	    "SELECT * FROM runners WHERE id BETWEEN ? and ?", req.query.min || 0, req.query.max || 10)
+	parseData(req, res, rows);
+});
 
 router.get("/login", authenticateToken, (req, res) => { parseData(req, res, req.user); });
 
 router.post("/login", async (req, res, next) => {
 	const user = await auth.get("SELECT * FROM users WHERE username = ?", req.body.username);
-		// Query the db for the given username
-		if (!user)
-			return next(createError(403, "User not found"));
-		// Apply the same algorithm to the POSTed password, applying
-		// the hash against the pass / salt, if there is a match we
-		// found the user
-		hash({password: req.body.password, salt: user.salt},
-			(err: Error, _pass: string, _salt: string, hash: string) => {
-				if (err)
-					return next(err);
+	// Query the db for the given username
+	if (!user)
+		return next(createError(403, "User not found"));
+	// Apply the same algorithm to the POSTed password, applying
+	// the hash against the pass / salt, if there is a match we
+	// found the user
+	hash({password: req.body.password, salt: user.salt},
+	    (err: Error, _pass: string, _salt: string, hash: string) => {
+		    if (err)
+			    return next(err);
 
-				if (hash === user.password) {
-					const token = generateAccessToken(req.body.username);
-					parseData(req, res, {token: token});
-					return;
-				}
+		    if (hash === user.password) {
+			    const token = generateAccessToken(req.body.username);
+			    parseData(req, res, {token: token});
+			    return;
+		    }
 
-				parseData(req, res, {error: "Wrong passowrd"});
-			});
-	});
+		    parseData(req, res, {error: "Wrong passowrd"});
+	    });
+});
 
 router.post("/run/add", authenticateToken, async (req, res, next) => {
 	const run = req.body;
@@ -128,44 +126,45 @@ router.post("/run/add", authenticateToken, async (req, res, next) => {
 
 	// All input from the client comes as a string, so everything must be parsed as an int
 	run.time = parseInt(run.hour, 10) * 60 * 60 + parseInt(run.minutes, 10) * 60
-			   + parseInt(run.seconds, 10);
+	           + parseInt(run.seconds, 10);
 
 	// Add 0.0001 to the end of runs that time with milliseconds
 	// This ensures that the site will display 3 significant figures
 	if (parseInt(run.milliseconds, 10))
 		run.time += parseInt(run.milliseconds, 10) / 1000 + 0.0001;
 
-		await leaderboard.run("INSERT INTO runs VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
-			
-				run.category_id,
-				run.date,
-				run.time,
-				// Run duration
-				0,
-				run.platform,
-				run.seed,
-				run.version,
-				run.input,
-				run.link,
-				// WR flag
-				0,
-		);
+	await leaderboard.run(
+	    "INSERT INTO runs VALUES(NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
 
-		// Prevents DoS
-		if (!(run.runners instanceof Array))
-			return [];
+	    run.category_id,
+	    run.date,
+	    run.time,
+	    // Run duration
+	    0,
+	    run.platform,
+	    run.seed,
+	    run.version,
+	    run.input,
+	    run.link,
+	    // WR flag
+	    0,
+	);
 
-		// Insert the run/runner pairs
-			for (const runner of run.runners) 
-				await leaderboard.run(`INSERT INTO pairs VALUES(
+	// Prevents DoS
+	if (!(run.runners instanceof Array))
+		return [];
+
+	// Insert the run/runner pairs
+	for (const runner of run.runners)
+		await leaderboard.run(`INSERT INTO pairs VALUES(
 					(SELECT MAX(id) FROM runs),
 					(SELECT id FROM runners WHERE name = ?)
 					)`,
-					runner);
-					
+		    runner);
 
-		// Update the WR flags
-		await leaderboard.run(`UPDATE runs
+
+	// Update the WR flags
+	await leaderboard.run(`UPDATE runs
 						SET wr = CASE WHEN time = (
 								SELECT time FROM runs
 								WHERE category_id = ?
@@ -173,41 +172,41 @@ router.post("/run/add", authenticateToken, async (req, res, next) => {
 						)
 						THEN 1 ELSE 0
 						END`,
-			run.category_id);
+	    run.category_id);
 
-		// Calculate the duration of each run in the category just updated
-		const rows = await leaderboard.all(`SELECT id, date, time FROM runs
+	// Calculate the duration of each run in the category just updated
+	const rows = await leaderboard.all(`SELECT id, date, time FROM runs
 						WHERE category_id = ?
 						ORDER BY date ASC`,
-			run.category_id);
-				for (let i = 0, len = rows.length; i < len; i++) {
-					// Check every newer record until a faster one is found
-					// Can't just check the very next because of ties
-					for (let j = i + 1; j <= len; j++) {
-						// Check if the record is current
-						if (j === len) {
-							rows[i].duration = Date.now() / 1000 - rows[i].date;
-						} else if (rows[j].time != rows[i].time) {
-							rows[i].duration = rows[j].date - rows[i].date;
-							break;
-						}
-					}
+	    run.category_id);
+	for (let i = 0, len = rows.length; i < len; i++) {
+		// Check every newer record until a faster one is found
+		// Can't just check the very next because of ties
+		for (let j = i + 1; j <= len; j++) {
+			// Check if the record is current
+			if (j === len) {
+				rows[i].duration = Date.now() / 1000 - rows[i].date;
+			} else if (rows[j].time != rows[i].time) {
+				rows[i].duration = rows[j].date - rows[i].date;
+				break;
+			}
+		}
 
-					// Add the runs duration to the DB
-					await leaderboard.run(`UPDATE runs SET duration = ? WHERE id = ?`,
-						rows[i].duration, rows[i].id);
-				}
+		// Add the runs duration to the DB
+		await leaderboard.run(
+		    `UPDATE runs SET duration = ? WHERE id = ?`, rows[i].duration, rows[i].id);
+	}
 
-		const categories = await leaderboard.all("SELECT * FROM categories");
-			res.render("admin_add", {
-				banner: {
-					text: "Run added succesfully",
-					status: "success",
-					csrfToken: req.csrfToken(),
-				},
-				categories: categories,
-				csrfToken: req.csrfToken(),
-			});
-		});
+	const categories = await leaderboard.all("SELECT * FROM categories");
+	res.render("admin_add", {
+		banner: {
+			text: "Run added succesfully",
+			status: "success",
+			csrfToken: req.csrfToken(),
+		},
+		categories: categories,
+		csrfToken: req.csrfToken(),
+	});
+});
 
 export default router;
