58e0f4c1a8aed191e10e2abb09ae3494db8daf82
Mango0x45 <thomasvoss@live.com>
2021-01-16 20:06:43 +0100

    Check for uninstalled deps

--- src/utils/format.sh
+++ src/utils/format.sh
@@ -3,10 +3,15 @@
 # Format all typescript/javascript files with clang-format
 # and use tabs instead of spaces in shell scripts
 
+not_installed() {
+	echo "Error: package '$1' required for formatting not found"
+	exit 1
+}
+
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 
 for FILE in $(find "$SCRIPT_PATH/../../" | grep "\.[jt]s$" | grep -v "node_modules"); do
-	clang-format --verbose -i -style=file "$FILE"
+	clang-format --verbose -i -style=file "$FILE" || not_installed clang-format
 done
 
 # Unexpanding and redirecting into the same file erases
@@ -15,7 +20,7 @@ done
 # executable again.
 for FILE in "$SCRIPT_PATH"/*.sh; do
 	echo Formatting "$FILE"
-	unexpand -t 4 --first-only "$FILE" >temp
+	unexpand -t 4 --first-only "$FILE" >temp || not_installed unexpand
 	mv temp "$FILE"
 	chmod +x "$FILE"
 done
--- src/utils/imageopt.sh
+++ src/utils/imageopt.sh
@@ -2,9 +2,14 @@
 
 # Optimze the images used on the site
 
+not_installed() {
+    echo "Error: package 'jpegtran' required for image optimization not found"
+    exit 1
+}
+
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$SCRIPT_PATH"/../public/cdn/assets || exit 1
 
 for FILE in *.jpg; do
-    jpegtran -optimize -outfile "$FILE" -copy none -perfect -v "$FILE"
+    jpegtran -optimize -outfile "$FILE" -copy none -perfect -v "$FILE" || not_installed
 done
