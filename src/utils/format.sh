f56164d5f7be4c2938e8624bb113682543e1edef
Mango0x45 <thomasvoss@live.com>
2021-02-27 00:18:14 +0100

    I live and breath efficiency

--- src/utils/format.sh
+++ src/utils/format.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Format all typescript/javascript files with clang-format
-# and use tabs instead of spaces in shell scripts
+# and use tabs instead of spaces in shell scripts.
 
 not_installed() {
 	echo "Error: package '$1' required for formatting not found"
@@ -10,14 +10,14 @@ not_installed() {
 
 SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 
-for FILE in $(find "$SCRIPT_PATH/../../" | grep "\.[jt]s$" | grep -v "node_modules"); do
+# This will break if a filename has spaces, but we don't like spaces!
+for FILE in $(find ./src -regex '.*\.[jt]s$'); do
 	clang-format --verbose -i -style=file "$FILE" || not_installed clang-format
 done
 
-# Unexpanding and redirecting into the same file erases
-# all the contents, so we redirect into a temporary file
-# and then rename it, but that requires marking it as
-# executable again.
+# Unexpanding and redirecting into the same file erases all the contents, so we
+# redirect into a temporary file and then rename it, but that requires marking
+# it as executable again.
 for FILE in "$SCRIPT_PATH"/*.sh; do
 	echo Formatting "$FILE"
 	unexpand -t 4 --first-only "$FILE" >temp || not_installed unexpand
