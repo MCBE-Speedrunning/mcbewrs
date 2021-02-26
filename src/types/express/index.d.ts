b36960f05c1c2a4970d34368263f15b267a2d17c
Mango0x45 <thomasvoss@live.com>
2021-02-27 00:13:53 +0100

    Move `types/` out of `utils/`

---
+++ src/types/express/index.d.ts
@@ -0,0 +1,19 @@
+declare namespace Express {
+	interface Request {
+		user: any;
+		username: string;
+		session: Session;
+	}
+
+	interface Session {
+		user: any;
+		success: string;
+		error: string;
+		regenerate(fn: () => void): Callback;
+		destroy(fn: () => void): Callback;
+	}
+
+	interface Callback {
+		(): void;
+	}
+}
---
+++ src/types/runs.ts
@@ -0,0 +1,10 @@
+declare namespace Runs {
+	type Duration = number|string;
+
+	interface Timestamp {
+		date: number;
+		beg: number;
+	}
+}
+
+export default Runs;
